277714d8fddbe1ae5857a979a5f0dffcc7f99285
Bohdan Basov <basovbohdan@gmail.com>
2017-01-28 12:10:17 +0000

    Install Bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,12 +10,11 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    
   end
   
   def create
     @article = Article.new(article_params)
-    
+
     if @article.save
       flash[:notice] = "Article was successfully created"
       redirect_to article_path(@article)
@@ -25,8 +24,6 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    
-    
     if @article.update(article_params)
       flash[:notice] = "Article successfully updated"
       redirect_to article_path(@article)
@@ -36,11 +33,9 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    
   end
   
   def destroy
-    
     @article.destroy
     flash[:notice] = "Article was successfully deleted"
     redirect_to articles_path
